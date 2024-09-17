9632abfbbdde26d80be4828396eab51b87dbe5b4
Einar Gatchalian <einar.gatchalian@gmail.com>
2024-09-17 14:46:45 -0700

    Python API for pricing

---
+++ AmazonPricingAPI.py
@@ -0,0 +1,19 @@
+import boto3, json
+from botocore.config import Config
+
+# Configuration for the AWS Pricing API client
+my_config = Config(
+    region_name='us-east-1',
+    retries={'max_attempts': 10}
+)
+
+client = boto3.client('pricing', config=my_config)
+
+# Retrieve service details
+# response = client.describe_services(ServiceCode='AmazonEC2') 
+
+# Retrieve product details
+response = client.get_products(ServiceCode='AmazonEC2') 
+
+with open("amazon.json", "w") as file:
+    json.dump(response, file)
