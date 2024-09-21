8da65d0bd9e6e867a8cf73cd40efb06f22cc40c7
Einar Gatchalian <einar.gatchalian@gmail.com>
2024-09-20 23:08:24 -0700

    updated to try to use requests

--- GooglePricingAPI.py
+++ GooglePricingAPI.py
@@ -1,37 +1,32 @@
 from google.oauth2 import service_account
 from google.cloud import billing_v1
-import json
+from google.auth import default
+from google.protobuf.json_format import MessageToDict
+import json, requests
+import requests
+
 key_path = '/Users/egatchal/CloudExpert/pricingcheck-436202-f19db4cf7f31.json'
 
 credentials = service_account.Credentials.from_service_account_file(key_path)
-client = billing_v1.CloudCatalogClient(credentials=credentials)
+print(credentials.token)
+# client = billing_v1.CloudCatalogClient(credentials=credentials)
 
-def list_compute_skus():
-    # Google Compute Engine service ID
-    compute_service_id = '6F81-5844-456A'
-    parent = f'services/{compute_service_id}'
-    
-    # Retrieve SKUs for the Compute Engine service
-    skus = client.list_skus(parent=parent)
-   
-    with open("GoogleSKUs.json", "w") as file:
-        json.dump(skus, file)
-    # for sku in skus:
-    #     print(sku)
-    #     # print(f'SKU ID: {sku.id}')
-    #     # print(f'Display Name: {sku.display_name}')
-    #     # for pricing_info in sku.pricing_info:
-    #     #     print(f' - Pricing Expression: {pricing_info.pricing_expression}')
-    #     #     for tier in pricing_info.pricing_expression.tiers:
-    #     #         print(f'   - Tier: {tier.start_unit} - {tier.end_unit} at {tier.unit_price.nanos / 1e9} {tier.unit_price.currency_code}')
+# Set the service ID
+service_id = 'services/6F81-5844-456A'
 
-def list_services():
-    services = client.list_services()
-    
-    with open("GoogleServices.json", "w") as file:
-        json.dump(services, file)
+# Prepare the request
+url = f'https://cloudbilling.googleapis.com/v1/{service_id}/skus'
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Accept': 'application/json'
+}
 
-# Call the function to list services
-# list_services()
+# Make the request
+response = requests.get(url, headers=headers)
 
-list_compute_skus()
+# Check the response
+if response.status_code == 200:
+    skus = response.json()
+    print(skus)
+else:
+    print(f'Error: {response.status_code} - {response.text}')
