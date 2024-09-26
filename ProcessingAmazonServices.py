c3521eef7e984d064cd2f89f927d1e0bb45aa2b9
Einar Gatchalian <einar.gatchalian@gmail.com>
2024-09-25 23:22:56 -0700

    finding attributes for AWS

--- ProcessingAmazonServices.py
+++ ProcessingAmazonServices.py
@@ -3,9 +3,31 @@ Python script to extract certain features from AWS EC2 Instance data
 """
 import json
 f = open("AmazonServices.json", "r+")
-
 services = json.load(f)
+f.close()
+# print(services[0].keys())
+
 print(services[0]["serviceCode"])
+
 print(services[0]["product"]["attributes"].keys())
 
-f.close()
+# print(services[0]["terms"])
+
+# print(services[0]["version"])
+
+# print(services[0]["publicationDate"])
+
+"""
+product
+    productFamily
+    sku
+    attributes
+        vcpu
+        memory
+        dedicatedEbsThoughput
+        operatingSystem
+        regionCode
+        processorArchitecture
+        instanceType
+
+"""
