ddbd90006c7e3557dd63e48c7c1b3816e4026fad
Einar Gatchalian <einar.gatchalian@gmail.com>
2024-09-29 22:22:12 -0700

    printing azure file and loading data

--- ProcessingAzureServices.py
+++ ProcessingAzureServices.py
@@ -3,8 +3,7 @@ Python script to extract certain features from Azure Virtual Machines data
 """
 import json
 f = open("AzureServices.json", "r+")
-
 data = json.load(f)
-services = data["Items"]
-
 f.close()
+services = data["Items"]
+print(services)
