b8f325497079c2ee36c6d7a18f192d1522963bb9
Einar Gatchalian <einar.gatchalian@gmail.com>
2024-10-01 23:00:09 -0700

    Azure document update

--- ProcessingAzureServices.py
+++ ProcessingAzureServices.py
@@ -1,5 +1,8 @@
 """
 Python script to extract certain features from Azure Virtual Machines data
+Will need to extract more information from Azure Virtual Machines
+Need data on VM, cpus, memory, etc
+Use other API to obtain data about a VM with a specific ID
 """
 import json
 f = open("AzureServices.json", "r+")
